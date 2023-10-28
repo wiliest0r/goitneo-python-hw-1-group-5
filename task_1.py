bb9b4d44a77c7fa0527a1988b0dfa3a0d4fa2811
wiliest0r <144823680+wiliest0r@users.noreply.github.com>
2023-10-28 14:31:34 +0000

    Init data

---
+++ task_1.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta, date
+
+users = [
+    {
+        "name": "Belly Taes",      
+        "birthday": datetime(1995, 1,   25)
+        },
+    {
+        "name": "Tom Sonl",        
+        "birthday": datetime(1989, 2,   21)
+        },
+    {
+        "name": "Joshua Harison",  
+        "birthday": datetime(1997, 8,   9)
+        },
+    {
+        "name": "Tony Lourens",    
+        "birthday": datetime(2003, 10,   26)
+        },
+    {
+        "name": "Jacob Marxon",    
+        "birthday": datetime(2001, 11,  8)
+        },
+    {
+        "name": "Bill Gates",      
+        "birthday": datetime(1955, 10,  28)
+        }
+]
